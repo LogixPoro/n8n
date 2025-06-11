7f86ece14c0e31e3ec688b308b73d48f6abf4425
LogixPoro <76068895+LogixPoro@users.noreply.github.com>
2025-06-11 18:47:21 +0300

    Update docker-entrypoint.sh

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-if [ -d /opt/custom-certificates ]; then
-  echo "Trusting custom certificates from /opt/custom-certificates."
-  export NODE_OPTIONS="--use-openssl-ca $NODE_OPTIONS"
-  export SSL_CERT_DIR=/opt/custom-certificates
-  c_rehash /opt/custom-certificates
-fi
 
-if [ "$#" -gt 0 ]; then
-  # Got started with arguments
-  exec n8n "$@"
-else
-  # Got started without arguments
-  exec n8n
-fi
+set -e
+
+# Add optional telemetry
+export N8N_VERSION=${N8N_VERSION:-unknown}
+export N8N_COMMIT_SHA=${N8N_COMMIT_SHA:-unknown}
+
+exec "$@"
